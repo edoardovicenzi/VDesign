78e6cec9ac5c4f3a04c886f889026b6a7af05574
Edoardo Vicenzi <edoardo.vicenzi@dedagroup.it>
2024-07-22 10:43:07 +0200

    STYLE refactor main.js | FEAT URL pathname for menu anchors (gh-pages)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,95 @@
+//Handle route (for github pages only)
+
+initializeMenuAnchorsHref()
+
 //Handle open/close nav bar
 
-document.querySelector('#toggle-nav').addEventListener('click', (e) => {
-    toggleNav(e)
-})
+initializeMenuListeners()
 
-document.querySelector('.main-navigation-backdrop').addEventListener('click', (e) => {
-    toggleNav(e)
-})
+//Populate the urls of the icons from json
 
-document.querySelectorAll('.main-navigation-list-item').forEach((element) => {
-    element.addEventListener('click', (e) => {
-        toggleNav(e)
-    })
-});
+populateSubsectionIcons()
+
+//Check if the page is under construction by evaluating if the .page element has or not children
+
+checkWorkInProgressPage()
+
+
+
+
+
+//Utility functions
+
+function initializeMenuAnchorsHref(){
+    try {
+        const anchors = document.querySelector('.main-navigation-list').querySelectorAll('li a')
+        for (const anchor of anchors){
+            anchor.href = window.location.pathname + anchor.href
+        }
+
+    } catch (error) {
+        console.error('In initializeMenuAnchorsHref:', error.message) 
+    }
+}
+
+function initializeMenuListeners(params) {
+   try {
+        document.querySelector('#toggle-nav').addEventListener('click', (e) => {
+            toggleNav(e)
+        })
+
+        document.querySelector('.main-navigation-backdrop').addEventListener('click', (e) => {
+            toggleNav(e)
+        })
+
+        document.querySelectorAll('.main-navigation-list-item').forEach((element) => {
+            element.addEventListener('click', (e) => {
+                toggleNav(e)
+            })
+        });
+   } catch (error) {
+        console.error('In initializeMenuListeners:', error.message) 
+   } 
+}
+
+function populateSubsectionIcons(){
+    try {
+        fetch('../assets/data/external-urls.json')
+            .then((response) => response.json())
+            .then((data) => {
+                //get all img in subsection-body, which are wrappers for the icon
+                for (const imgTag of document.querySelectorAll('.subsection-body a img')){
+                    let imageName = imgTag.src.match(/(?<name>\w+)\.svg/).groups.name.toLowerCase();
+                    imgTag.parentElement.setAttribute('href', data.filter((el) => el.name == imageName)[0].url??"")
+                }
+            })  
+            .catch((err) => console.error("Fetch request went wrong: ", err));
+
+    } catch (error) {
+        console.error('In populateSubsectionIcons:', error.message) 
+    }
+}
+
+function checkWorkInProgressPage(params) {
+    try {
+        const page = document.querySelector('.page')
+
+        if (!page.children.length){
+            page.innerHTML = "<div class='under-construction'></div>"
+        }
+    } catch (error) {
+        console.error('In checkWorkInProgressPage:', error.message) 
+    } 
+}
+
+
+//DOM functions
+
 function toggleNav(e){
     const navbar = document.querySelector('.main-navigation')
     navbar.classList.toggle('open')
 
     const backdrop = document.querySelector('.main-navigation-backdrop')
     backdrop.classList.toggle('open')
-}
-
-
-//Populate the urls of the icons from json
-fetch('../assets/data/external-urls.json')
-.then((response) => response.json())
-.then((data) => {
-        //get all img in subsection-body, which are wrappers for the icon
-        for (const imgTag of document.querySelectorAll('.subsection-body a img')){
-            let imageName = imgTag.src.match(/(?<name>\w+)\.svg/).groups.name.toLowerCase();
-            imgTag.parentElement.setAttribute('href', data.filter((el) => el.name == imageName)[0].url??"")
-            
-        }
-
-    })  
-.catch((err) => console.error("Fetch request went wrong: ", err));
-
-
-//Check if the page is under construction by evaluating if the .page element has or not children
-
-const page = document.querySelector('.page')
-
-if (!page.children.length){
-    page.innerHTML = "<div class='under-construction'></div>"
 }
 
