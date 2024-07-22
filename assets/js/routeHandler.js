95c633049bdb108cc62c5a2b00c957286dfde683
Edoardo Vicenzi <edoardo.vicenzi@dedagroup.it>
2024-07-22 12:20:48 +0200

    FIX added function to dynamically change base

--- assets/js/routeHandler.js
+++ assets/js/routeHandler.js
@@ -1,7 +1,7 @@
 
 //Handle route (for github pages only)
 
-initializeMenuAnchorsHref()
+// initializeMenuAnchorsHref()
 
 function initializeMenuAnchorsHref(){
     try {
@@ -14,4 +14,28 @@ function initializeMenuAnchorsHref(){
     } catch (error) {
         console.error('In initializeMenuAnchorsHref:', error.message) 
     }
+}
+
+
+addEventListener("DOMContentLoaded", (event) => {
+    if (window.location.origin.indexOf('github.io') > 0){
+        initializeGithubBase('edoardovicenzi', 'VDesign')
+    }
+});
+
+
+
+function initializeGithubBase(githubName = '', githubRepoName = '') {
+    try {
+        if ((githubName && githubRepoName) && (typeof githubName == 'string' && typeof githubRepoName == 'string')){
+            const baseTag = document.createElement('base')
+            baseTag.href=`https://${githubName}.github.io/${githubRepoName}/`
+            document.querySelector('head').appendChild(baseTag)
+        }
+        else throw new TypeError('TypeError: Wrong parameter type passed or missing.')
+        
+    } catch (error) {
+        console.assert(githubName && githubRepoName,'In initializeBase.\nExited with error message'+ error.message +'\nEntered with value:',`\nGithub Name: ${githubName ? githubName :'__MISSING__'}`,`\nGithub Repository Name: ${githubRepoName? githubRepoName : 'MISSING'}` )
+    }
+    
 }
