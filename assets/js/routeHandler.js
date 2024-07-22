2d1b810ca75f2029f377c1160e89e21b5f356505
Edoardo Vicenzi <edoardo.vicenzi@dedagroup.it>
2024-07-22 12:40:15 +0200

    FIX html base now toggles if on github pages or in dev (localhost)

--- assets/js/routeHandler.js
+++ assets/js/routeHandler.js
@@ -1,29 +1,29 @@
-
 //Handle route (for github pages only)
 
-// initializeMenuAnchorsHref()
 
-function initializeMenuAnchorsHref(){
-    try {
-        if (window.location.origin.indexOf('github') > 0){
-            const anchors = document.querySelector('.main-navigation-list').querySelectorAll('li a')
-            for (const anchor of anchors){
-                anchor.href = window.location.origin + '/VDesign' + anchor.getAttribute('href')
-            }
-        }
-    } catch (error) {
-        console.error('In initializeMenuAnchorsHref:', error.message) 
-    }
-}
+//Change only this 2 constants to get it working
 
+const GITHUB_NAME = 'edoardovicenzi'
+const GITHUB_REPOSITORY_NAME = 'VDesign'
 
 addEventListener("DOMContentLoaded", (event) => {
+
+    //Check if we are using github pages
     if (window.location.origin.indexOf('github.io') > 0){
-        initializeGithubBase('edoardovicenzi', 'VDesign')
+        initializeGithubBase(GITHUB_NAME, GITHUB_REPOSITORY_NAME)
+    }
+    //Otherwise setup for local dev environment
+    else{
+        initializeBase()
     }
 });
 
-
+// Append the base child on the head
+function initializeBase() {
+        const baseTag = document.createElement('base')
+        baseTag.href= '/'
+        document.querySelector('head').appendChild(baseTag)
+}
 
 function initializeGithubBase(githubName = '', githubRepoName = '') {
     try {
@@ -37,5 +37,4 @@ function initializeGithubBase(githubName = '', githubRepoName = '') {
     } catch (error) {
         console.assert(githubName && githubRepoName,'In initializeBase.\nExited with error message'+ error.message +'\nEntered with value:',`\nGithub Name: ${githubName ? githubName :'__MISSING__'}`,`\nGithub Repository Name: ${githubRepoName? githubRepoName : 'MISSING'}` )
     }
-    
 }
